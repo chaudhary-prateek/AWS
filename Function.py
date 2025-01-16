d7701c34eac6919bb49f6b5e3a2d0b5838dee1ec
chaudhary-prateek <prateekchaudhary7805@gmail.com>
2025-01-16 10:47:14 +0530

    First commit

---
+++ Function.py
@@ -0,0 +1,24 @@
+# def myfunction(fname):
+#     print(fname + "MY")
+# myfunction("Email")
+# myfunction("Tobias")
+# myfunction("Linus")
+
+
+# import time
+# timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+
+# intro = "My name is {} and i'm from {}"
+
+
+# name = "Prateek"
+# add = "UP"
+# print(f"My name is {name} and i'm from {add}")
+
+# x = int(input("Enter a valid no. between 0 to 9 : "))
+# if (x<0 or x>9):
+#     raise ValueError("Enter a valid no.")
+# print(x)
+import math
+print(dir(math))
